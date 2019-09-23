ff42209d42e56b1c7d110a2eb3f72f1322d2ddb7
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-09-23 07:26:56 +0300

    urls

--- reversal.php
+++ reversal.php
@@ -11,16 +11,16 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'A
 $curl_post_data = array(
     //Fill in the request parameters with valid values
    // 'CommandID' => ' ',
-    'Initiator' => ' ',//This is the credential/username used to authenticate the transaction request.
-    'SecurityCredential' => ' ',//Base64 encoded string of the M-Pesa short code and password,
+    'Initiator' => 'testapi',//This is the credential/username used to authenticate the transaction request.
+    'SecurityCredential' => 'V8sAKPlxqhT+1y52nkMwjJQu/fF/F5JR/Xg89gRyZos1ic8gi1osh9cfvBasxdRuWmwd/sZ5ADL51RMExYTi2f117pix41sbXXZpf8vY2nvFM3fxaF/4fD4Pdij9UGPMY0rhheYt5Heno+wBn31JxS5IeBfuxXywnn7uty3hVEdlt8WOHWW4lj3/QlFAQla+8ktaFBTPCxw4yhmyVmIqZb/3VxxBcZN1Jk13hXAvea3SrMQ7HnmEKskQp+UDDzFN1iUsKai34gMGHVIyFwQS43B4/R07M/E83ZoisS7UDWzyni44bRtF95srrr1Oa9dm54dW4l+orfMp7zlqF1GbAA==',//Base64 encoded string of the M-Pesa short code and password,
     'CommandID' => 'TransactionReversal',//Unique command for each transaction type,
     'TransactionID' => ' ',//Organization Receiving the funds.
     'Amount' => ' ',
     'ReceiverParty' => ' ',
     'RecieverIdentifierType' => '4',//Type of organization receiving the transaction.
-    'ResultURL' => 'https://ip_address:port/result_url',//The path that stores information of transaction.
-    'QueueTimeOutURL' => 'https://ip_address:port/timeout_url',//The path that stores information of time out transaction.
-    'Remarks' => ' ',//Comments that are sent along with the transaction.
+    'ResultURL' => 'http://jochebedscrib.org/victor/result.php',//The path that stores information of transaction.
+    'QueueTimeOutURL' => 'http://jochebedscrib.org/victor/time_out.php',//The path that stores information of time out transaction.
+    'Remarks' => 'Reversal',//Comments that are sent along with the transaction.
     //'Occasion' => ' '//optional
 );
 
--- transactionStatus.php
+++ transactionStatus.php
@@ -10,14 +10,14 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'A
 
 $curl_post_data = array(
     //Fill in the request parameters with valid values
-    'Initiator' => ' ',//	The name of Initiator to initiating the request.
-    'SecurityCredential' => ' ',//Base64 encoded string of the M-Pesa short code and password,
+    'Initiator' => 'testapi',//	The name of Initiator to initiating the request.
+    'SecurityCredential' => 'V8sAKPlxqhT+1y52nkMwjJQu/fF/F5JR/Xg89gRyZos1ic8gi1osh9cfvBasxdRuWmwd/sZ5ADL51RMExYTi2f117pix41sbXXZpf8vY2nvFM3fxaF/4fD4Pdij9UGPMY0rhheYt5Heno+wBn31JxS5IeBfuxXywnn7uty3hVEdlt8WOHWW4lj3/QlFAQla+8ktaFBTPCxw4yhmyVmIqZb/3VxxBcZN1Jk13hXAvea3SrMQ7HnmEKskQp+UDDzFN1iUsKai34gMGHVIyFwQS43B4/R07M/E83ZoisS7UDWzyni44bRtF95srrr1Oa9dm54dW4l+orfMp7zlqF1GbAA==',//Base64 encoded string of the M-Pesa short code and password,
     'CommandID' => 'TransactionStatusQuery',//Unique command for each transaction type,
     'TransactionID' => ' ',//Organization Receiving the funds.
     'PartyA' => ' ',//Organization /MSISDN sending the transaction.
     'IdentifierType' => '1',//Type of organization receiving the transaction
-    'ResultURL' => 'https://ip_address:port/result_url',//The path that stores information of transaction.
-    'QueueTimeOutURL' => 'https://ip_address:port/timeout_url',//The path that stores information of time out transaction.
+    'ResultURL' => 'http://jochebedscrib.org/victor/result.php',//The path that stores information of transaction.
+    'QueueTimeOutURL' => 'http://jochebedscrib.org/victor/time_out.php',//The path that stores information of time out transaction.
     'Remarks' => 'Checking status of this transaction'//Comments that are sent along with the transaction.
     //'Occasion' => ' '//optional
 );
