7a5756051cdcacbb43b67cf04a4bd6d508a88380
ngangavic <ngangavictor10@gmail.com>
2020-10-03 20:06:32 +0300

    display access token

--- database/auth/access-token.php
+++ database/auth/access-token.php
@@ -2,15 +2,17 @@
 
 require "secrets.php";
 
+$headers=['Content-Type:application/json; charset=utf8'];
 $url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-  
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-$credentials = base64_encode($YOUR_APP_CONSUMER_KEY.':'.$YOUR_APP_CONSUMER_SECRET);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials)); //setting a custom header
-curl_setopt($curl, CURLOPT_HEADER, true);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$curl = curl_init($url);
+curl_setopt($curl,CURLOPT_HTTPHEADER,$headers);
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 
-$curl_response = curl_exec($curl);
+curl_setopt($curl,CURLOPT_HEADER,false);
 
-echo json_decode($curl_response);
+curl_setopt($curl,CURLOPT_USERPWD,$YOUR_APP_CONSUMER_KEY.':'.$YOUR_APP_CONSUMER_SECRET);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl,CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+$access_token = $result->access_token;
+echo 'The access token is: '.$access_token.'</br> Status: '.$status;
