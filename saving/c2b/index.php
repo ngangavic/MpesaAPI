4963c5278e628d967be4a9741843fe7ca68285a7
ngangavic <ngangavictor10@gmail.com>
2020-10-04 22:03:32 +0300

    save c2b response to database

--- saving/c2b/index.php
+++ saving/c2b/index.php
@@ -2,6 +2,8 @@
 
 require "../auth/access-token.php";
 
+require "../connection.php";
+
 $url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate';
   
 $curl = curl_init();
@@ -27,6 +29,12 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 $curl_response = curl_exec($curl);
 print_r($curl_response);
 
+$decode=json_decode($curl_response);
+
+$ConversationID=$decode->ConversationID;
+$OriginatorCoversationID=$decode->OriginatorCoversationID;
+$ResponseDescription=$decode->ResponseDescription;
+
 /******* log the response**********/
 $logFile = "../logs/c2bResponse.txt";
 // write the M-PESA Response to file
@@ -34,4 +42,9 @@ $log = fopen($logFile, "a");
 fwrite($log, $curl_response);
 fclose($log);
 
-echo $curl_response;
+$stmt=$connection->prepare("INSERT INTO tbl_c2b(ConversationID,OriginatorCoversationID,ResponseDescription)VALUES(?,?,?)");
+$stmt->bind_param("sss",$ConversationID,$OriginatorCoversationID,$ResponseDescription);
+$stmt->execute();
+$stmt->close();
+$connection->close();
+echo 'Saved';
