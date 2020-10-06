65080f8e0d480581c48884f11962c4cf64e91600
ngangavic <ngangavictor10@gmail.com>
2020-10-06 14:05:41 +0300

    saving callback response successfull

--- saving/lnmp/lnmp_callback.php
+++ saving/lnmp/lnmp_callback.php
@@ -4,52 +4,48 @@ require "../connection.php";
 
 $callBackResponse = file_get_contents('php://input');
 
-$decode=json_decode($callBackResponse);
+$logFile = '../logs/lnmpCallBackResponse.txt';
+$log = fopen($logFile, "a");
+fwrite($log, $callBackResponse . "\n");
+fclose($log);
 
-$resultCode=$decode->Body->stkCallback->ResultCode;
-$merchantRequestID=$decode->Body->stkCallback->MerchantRequestID;
-$checkoutRequestID=$decode->Body->stkCallback->CheckoutRequestID;
-$resultDesc=$decode->Body->stkCallback->ResultDesc;
+$decode = json_decode($callBackResponse);
 
-if($resultCode==0){
+$resultCode = $decode->Body->stkCallback->ResultCode;
+$merchantRequestID = $decode->Body->stkCallback->MerchantRequestID;
+$checkoutRequestID = $decode->Body->stkCallback->CheckoutRequestID;
+$resultDesc = $decode->Body->stkCallback->ResultDesc;
 
-    $amount=$decode->Body->CallbackMetadata->Item[0]->Value;
-    $mpesaReceiptNumber=$decode->Body->CallbackMetadata->Item[1]->Value;
-    $balance=$decode->Body->CallbackMetadata->Item[2]->Value;
-    $transactionDate=$decode->Body->CallbackMetadata->Item[3]->Value;
-    $phoneNumber=$decode->Body->CallbackMetadata->Item[4]->Value;
+if ($resultCode == 0) {
 
-    $toSave=$amount."\n".$mpesaReceiptNumber."\n".$balance."\n".$transactionDate."\n".$phoneNumber;
+    $amount = $decode->Body->stkCallback->CallbackMetadata->Item[0]->Value;
+    $mpesaReceiptNumber = $decode->Body->stkCallback->CallbackMetadata->Item[1]->Value;
+    $transactionDate = $decode->Body->stkCallback->CallbackMetadata->Item[3]->Value;
+    $phoneNumber = $decode->Body->stkCallback->CallbackMetadata->Item[4]->Value;
+    $balance = 0;
+
+    $toSave = $amount . "\n" . $mpesaReceiptNumber . "\n" . $transactionDate . "\n" . $phoneNumber;
 
     $logSFile = '../logs/lnmpCallBackResponseSuccess.txt';
-$logS = fopen($logSFile,"a");
-fwrite($logS,$toSave."\n");
-fclose($logS);
+    $logS = fopen($logSFile, "a");
+    fwrite($logS, $toSave . "\n");
+    fclose($logS);
 
-    $stmt=$connection->prepare("INSERT INTO tbl_lnmp_callback_success(MerchantRequestID, CheckoutRequestID, ResultCode, ResultDesc, Amount, MpesaReceiptNumber, Balance, TransactionDate, PhoneNumber) VALUES (s,s,s,s,s,s,s,s,s)");
-    $stmt->bind_param("ssssssss",$merchantRequestID,$checkoutRequestID,$resultCode,$resultDesc,$amount,$mpesaReceiptNumber,$balance,$transactionDate,$phoneNumber);
+    $stmt = $connection->prepare("INSERT INTO tbl_lnmp_callback_success(MerchantRequestID, CheckoutRequestID, ResultCode, ResultDesc, Amount, MpesaReceiptNumber, Balance, TransactionDate, PhoneNumber) VALUES (?,?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("sssssssss", $merchantRequestID, $checkoutRequestID, $resultCode, $resultDesc, $amount, $mpesaReceiptNumber, $balance, $transactionDate, $phoneNumber);
     $stmt->execute();
     $stmt->close();
     $connection->close();
-
-
-}else{
+} else {
 
     $logFileE = '../logs/lnmpCallBackResponseError.txt';
-$logE = fopen($logFileE,"a");
-fwrite($log,"ERROR"."\n");
-fclose($log);
+    $logE = fopen($logFileE, "a");
+    fwrite($log, "ERROR" . "\n");
+    fclose($log);
 
-    $stmt=$connection->prepare("INSERT INTO tbl_lnmp_callback_error(MerchantRequestID, CheckoutRequestID, ResultCode, ResultDesc) VALUES (s,s,s,s)");
-    $stmt->bind_param("ssss",$merchantRequestID,$checkoutRequestID,$resultCode,$resultDesc);
+    $stmt = $connection->prepare("INSERT INTO tbl_lnmp_callback_error(MerchantRequestID, CheckoutRequestID, ResultCode, ResultDesc) VALUES (?,?,?,?)");
+    $stmt->bind_param("ssss", $merchantRequestID, $checkoutRequestID, $resultCode, $resultDesc);
     $stmt->execute();
     $stmt->close();
     $connection->close();
-
 }
-
-
-$logFile = '../logs/lnmpCallBackResponse.txt';
-$log = fopen($logFile,"a");
-fwrite($log,$callBackResponse."\n");
-fclose($log);
